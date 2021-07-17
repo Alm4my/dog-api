2e93f19829bba33a29182dbd3242517ca523a917
Alm4my <thenixadm@gmail.com>
2021-07-17 00:47:21 +0000

    Initial Commit

---
+++ src/test/java/fr/almamy/dogapi/DogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.almamy.dogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
