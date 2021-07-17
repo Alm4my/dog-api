8dbcf46ea79363e33578b776f247c3edac82e198
Alm4my <thenixadm@gmail.com>
2021-07-17 03:38:59 +0000

    [BUGFIX] Fix bug with JPA not initializing data properly
    This add the spring.jpa.defer-datasource-initialization=true property to our application to allow JPA to defer data initialization which was creating a bug which did not permit to the data to be inserted into our database per our data.sql file.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (id, name, breed, origin)
+VALUES (1, 'Fluffy', 'Pomeranian', 'Mountain View, CA'),
+        (2, 'Spot', 'Pit Bull', 'Austin, TX'),
+        (3, 'Ginger', 'Cocker Spaniel', 'Kansas City, KS'),
+        (4, 'Lady', 'Direwolf', 'The North'),
+        (5, 'Sasha', 'Husky', 'Buffalo, NY');
