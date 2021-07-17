b33579d5ad38c7eb062fa2382e3f2a70090cd82a
Alm4my <thenixadm@gmail.com>
2021-07-17 01:36:35 +0000

    Add repository for querying the data from our database
    The CrudRepository<> implements a lot of crud operations for us. However, those that we need can either be specified like we did with the queries or we can use the built-in queries from CrudRepository within DogService to get all dogs or get a dog by ID, then process the resulting object as well.

---
+++ src/main/java/fr/almamy/dogapi/repository/DogRepository.java
@@ -0,0 +1,22 @@
+package fr.almamy.dogapi.repository;
+
+import fr.almamy.dogapi.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DogRepository extends CrudRepository<Dog, Long> {
+    @Query("select d.id, d.breed from Dog d  where d.id=:id")
+    String findBreedById(Long id);
+
+    @Query("select d.id, d.breed from Dog d")
+    List<String> findAllBreed();
+
+    @Query("select d.id, d.name from Dog d")
+    List<String> findAllName();
+
+
+}
